9a92ed9e819cf678d4d393a838342e9e216becda
drummk2 <klrdrumm@outlook.com>
2025-04-26 03:21:55 +0100

    Added a basic footer to the overall layout.

--- personal-portfolio/src/App.js
+++ personal-portfolio/src/App.js
@@ -23,6 +23,23 @@ function App() {
                 <NavigationBar onChangePage={setPage}/>
             </div>
             <PageComponent />
+            <footer>
+                <hr className="footer-line" />
+                <div className="footer-content-container">
+                    <div className="footer-text-container">
+                        <p className="footer-text-header">Email</p>
+                        <p className="footer-text-content">klrdrumm@outlook.com</p>
+                    </div>
+                    <div className="footer-text-container">
+                        <p className="footer-text-header">LinkedIn</p>
+                        <p className="footer-text-content"><a href="https://www.linkedin.com/in/kieron-drumm-788382114/">https://www.linkedin.com/in/kieron-drumm-788382114/</a></p>
+                    </div>
+                    <div className="footer-text-container">
+                        <p className="footer-text-header">GitHub</p>
+                        <p className="footer-text-content"><a href="https://github.com/drummk2">https://github.com/drummk2</a></p>
+                    </div>
+                </div>
+            </footer>
         </div>
     );
 }
--- personal-portfolio/src/components/personal-image-description/PersonalImageDescription.js
+++ personal-portfolio/src/components/personal-image-description/PersonalImageDescription.js
@@ -8,8 +8,11 @@ const PersonalImageDescription = () => {
             <p className="personal-image-description-subheader">A Bit About Me</p>
             <p className="personal-image-description-bio">
                 I'm a full-stack software engineer at <a href="https://www.generait-doc.com">Generait AI Solutions</a>,
-                with extensive experience developing .NET, React, and AI-driven web and desktop applications. Outside of my primary role,
-                I'm also passionate about AI, with my previous research having focused on its potential applications in the field of genealogy.</p>
+                with extensive experience in developing AI-driven web and desktop applications using .NET, React, and a
+                wide range of Microsoft technologies — including Microsoft Dynamics 365 and the Power Platform. Outside of my
+                primary role, I'm passionate about artificial intelligence, and my previous research has explored its potential
+                applications in the field of genealogy.
+            </p>
         </>
     );
 };
