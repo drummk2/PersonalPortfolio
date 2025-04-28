ba4150435cbe5d1146065de6b6bedc4270146467
drummk2 <klrdrumm@outlook.com>
2025-04-28 10:44:42 +0100

    Added a placeholder image to the sections that have not yet been implemented.

--- personal-portfolio/src/components/experience-page-container/ExperiencePageContainer.tsx
+++ personal-portfolio/src/components/experience-page-container/ExperiencePageContainer.tsx
@@ -1,9 +1,14 @@
 import React from "react";
 import "./experience-page-container.css";
+import stayTunedImage from "../../assets/stay-tuned.jpg";
 
 const ExperiencePageContainer: React.FC = () => {
     return (
-        <div className="experience-page-container"></div>
+        <div className="experience-page-container">
+            <img
+                alt="Stay tuned"
+                src={stayTunedImage} />
+        </div>
     );
 };
 
--- personal-portfolio/src/components/projects-page-container/ProjectsPageContainer.tsx
+++ personal-portfolio/src/components/projects-page-container/ProjectsPageContainer.tsx
@@ -1,9 +1,14 @@
 import React from "react";
 import "./projects-page-container.css";
+import stayTunedImage from "../../assets/stay-tuned.jpg";
 
 const ProjectsPageContainer: React.FC = () => {
     return (
-        <div className="projects-page-container"></div>
+        <div className="projects-page-container">
+            <img
+                alt="Stay tuned"
+                src={stayTunedImage} />
+        </div>
     );
 };
 
